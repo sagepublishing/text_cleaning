4be871da9e75c6a0246f0dae2dd3bc7bd6b5d88e
alix-dumoulin <alix.dumoulin.16@ucl.ac.uk>
2020-07-09 11:00:26 +0200

    stopwords, whitespaces

--- app_scripts/clean/strip_whitespaces/__init__.py
+++ app_scripts/clean/strip_whitespaces/__init__.py
@@ -1,7 +1,7 @@
 import gensim
-from gensim.parsing.preprocessing import remove_stopwords
+from gensim.parsing.preprocessing import strip_multiple_whitespaces
 
-def remove_stopwords(inStr):
+def strip_whitespaces(inStr):
 	"""Filters out stopwords."""
 	filtered_string = strip_multiple_whitespaces(inStr)
 	return filtered_string
