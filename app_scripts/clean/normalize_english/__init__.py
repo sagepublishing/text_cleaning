f9f05994cb8aa651048bf95cbb651e82410e54f7
alix-dumoulin <alix.dumoulin.16@ucl.ac.uk>
2020-07-12 17:15:14 +0200

    lemmatize

--- app_scripts/clean/normalize_english/__init__.py
+++ app_scripts/clean/normalize_english/__init__.py
@@ -1,26 +1,39 @@
 import pandas as pd
+import re
 
-def normalize_english(inStr: str, opts: dict):
-	"""Normalizes enlish to the target language (UK or US) based
-	on the dictionary found here: http://www.tysto.com/uk-us-spelling-list.html"""
 
-	# import the matching table
-	df = pd.read_csv("us_uk_matching_table.csv")
+def normalize_english(inStr, target):
+    """Normalizes enlish to the target language (UK or US) based
+    on the dictionary found here: http://www.tysto.com/uk-us-spelling-list.html"""
 
-	# create dictionaries (faster to iterate on)
-	translate_uk_us = dict(zip(df.UK, df.US))
-	translate_us_uk = dict(zip(df.US, df.UK))
+    # import the matching table
+    df = pd.read_csv("https://raw.githubusercontent.com/sagepublishing/text_cleaning/app_prototype/app_scripts/clean/normalize_english/us_uk_matching_table.csv")
 
-	# get target language inputted by user
-	target = opts['target_language']
+    # create dictionaries (faster to iterate on)
+    translate_uk_us = dict(zip(df.UK, df.US))
+    translate_us_uk = dict(zip(df.US, df.UK))
 
-	# depending on target language, search and match relevant dict
-	if target == "UK":
-		for item in translate_us_uk.keys():
-			string = re.sub(item, translate_us_uk[item], inStr)
-		return string
+    # get target language inputted by user
+    #target = opts['target_language']
 
-	elif target =="US":
-		for item in translate_uk_us.keys():
-			string = re.sub(item, translate_uk_us[item], inStr)
-		return string
+    # depending on target language, search and match relevant dict
+    if target == "UK":
+        for item in translate_us_uk.keys():
+        	# the word has to be lower case to match the dictionary, but return the correct capitalization using ifelse statement
+            if inStr[0].isupper():
+                return re.sub(item, translate_us_uk[item], inStr.lower()).capitalize()
+            else:
+                return re.sub(item, translate_us_uk[item], inStr)
+
+    elif target =="US":
+        for item in translate_uk_us.keys():
+        	# the word has to be lower case to match the dictionary, but return the correct capitalization using ifelse statement
+            if inStr[0].isupper():
+                return re.sub(item, translate_uk_us[item], inStr.lower()).capitalize()
+            else:
+                return re.sub(item, translate_uk_us[item], inStr)
+            
+    else:
+    	# if input is neither US nor UK, return error message
+        return "Wrong input - please enter \"US\" or \"UK\"."
+  
