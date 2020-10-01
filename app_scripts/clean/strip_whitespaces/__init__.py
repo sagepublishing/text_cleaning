b2fd3905af0e3d56ebe11a21d266e879dc7cb863
danielagduca <daniela.duca@sagepub.co.uk>
2020-10-01 16:15:52 +0100

    Update __init__.py

--- app_scripts/clean/strip_whitespaces/__init__.py
+++ app_scripts/clean/strip_whitespaces/__init__.py
@@ -2,6 +2,6 @@ import gensim
 from gensim.parsing.preprocessing import strip_multiple_whitespaces
 
 def strip_whitespaces(inStr):
-	"""Filters out stopwords."""
+	"""Filters out multiple whitespaces."""
 	filtered_string = strip_multiple_whitespaces(inStr)
 	return filtered_string
