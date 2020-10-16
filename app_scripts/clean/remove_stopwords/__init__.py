3958ce8653396ee4a82ee8a4fa45b3de026df431
Indiiigo <indira15021@iiitd.ac.in>
2020-10-16 14:18:48 +0200

    * changed remove stopwords funnction name to prevent gensim clash

--- app_scripts/clean/remove_stopwords/__init__.py
+++ app_scripts/clean/remove_stopwords/__init__.py
@@ -1,7 +1,7 @@
 import gensim
 from gensim.parsing.preprocessing import remove_stopwords
 
-def remove_stopwords(inStr):
+def remove_default_stopwords(inStr):
 	"""Filters out stopwords."""
 	filtered_string = remove_stopwords(inStr)
 	return filtered_string
