482fb588a258e93e5665e5b44c257dd0fc0657f0
Indira Sen <indira15021@iiitd.ac.in>
2020-10-26 17:51:26 +0100

    Update __init__.py
    * small change to test webhook

--- app_scripts/clean/stem_words/__init__.py
+++ app_scripts/clean/stem_words/__init__.py
@@ -2,5 +2,5 @@ import gensim
 from gensim.parsing.preprocessing import stem_text
 
 def stem_words(inStr):
-	"""Stems words in text."""
+	"""Stems words in text using gensim."""
 	return stem_text(inStr)
