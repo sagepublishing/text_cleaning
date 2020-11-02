6558a0e2cc6a4cefd876741c9cd9bed8cb8e876f
danielagduca <daniela.duca@sagepub.co.uk>
2020-11-02 16:33:11 +0000

    adding logo to readthedocs

--- source/conf.py
+++ source/conf.py
@@ -48,8 +48,13 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme_options = {
+    'logo': 'SAGE-texti-logo.svg',
+    'github_repo': 'text_cleaning',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
